2cc53f839e0e3617fa6b9ef42a7a40d9935625bd
valentynbondiashev <valik520954@gmail.com>
2024-09-13 20:38:05 +0300

    nx config

--- packages/buttons/Button.tsx
+++ packages/buttons/Button.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export function Button(props: any) {
   const [count, setCount] = useState(0);
-  console.log('test', props);
+
   return (
     <button onClick={() => setCount((count) => count + 1)}>
       count is {count}
--- packages/forms/Form.tsx
+++ packages/forms/Form.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { Button } from '@tuskdesign/buttons';
+import { Button } from "@tuskdesign/buttons";
 
 export function TuskForm(props: any) {
-  return;
-  <>
-    <div>I'm supposed to be a form 🤷‍♂️</div>
-    <Button />
-  </>;
+  return (
+    <>
+      <div>I'm supposed to be a form 🤷‍♂️</div>
+      <Button />
+    </>
+  );
 }
 
 export default TuskForm;
